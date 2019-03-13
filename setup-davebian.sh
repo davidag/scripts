34c619fd170b7ace45230bb1d07fd5fb02beaf45
David Alfonso <developer@davidalfonso.es>
2019-03-13 18:27:46 +0100

    setup-davebian: add some more tools

--- setup-davebian.sh
+++ setup-davebian.sh
@@ -9,7 +9,7 @@ apt update && sudo apt dist-upgrade
 apt autoremove
 
 # etckeeper & basic tools
-apt install --yes etckeeper git vim sudo
+apt install --yes etckeeper git gitk vim sudo
 
 # network manager
 apt install --yes network-manager
@@ -26,12 +26,19 @@ apt install --yes libgtk-3-0 libdbus-glib-1-2 libavcodec58
 # keepass2
 apt install keepass2
 
+# file manager and pdf viewer
+apt install xfe xpdf
+
 # command-line tools
-apt install --yes tree unzip curl lsof
+apt install --yes tree unzip curl lsof colordiff p7zip unison dnsutils ccd2iso duc
 
 # debian tools
 # - debconf-utils: installs debconf-get-selections to automate package installation
 apt install debconf-utils
+# - apt-file: search files on any package (whether installed or not)
+#   The package cache must be updated after installing apt-file
+apt install apt-file
+apt update
 
 # vagrant with qemu-kvm
 apt install vagrant
@@ -45,3 +52,19 @@ apt install pulseaudio pavucontrol
 
 # avoid entering ssh keys on every terminal
 apt install keychain
+
+# protonvpn
+apt install openvpn dialog wget python
+wget -O protonvpn-cli.sh https://raw.githubusercontent.com/ProtonVPN/protonvpn-cli/master/protonvpn-cli.sh
+chmod +x protonvpn-cli.sh
+./protonvpn-cli.sh --install
+rm protonvpn-cli.sh
+
+# php & composer
+apt install php php-xml php-gd php-mbstring php-curl
+wget -O composer-setup.php https://getcomposer.org/installer
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm composer-setup.php
+
+# python3 development
+apt install python3 python3-venv
